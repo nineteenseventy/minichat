0efb0f5714c44795a557d5d4c89d779c240193d6
Marius Kehl <80584154+MeroFuruya@users.noreply.github.com>
2024-08-31 13:19:11 +0200

    chore: Update nuxt.config.ts with pinia alias

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: [
-    'nuxt-auth-utils',
-    '@nuxt/eslint',
-    '@nuxtjs/color-mode',
-    '@nuxtjs/device',
-    '@pinia/nuxt',
-    '@primevue/nuxt-module',
-    '@vueuse/nuxt',
+    "nuxt-auth-utils",
+    "@nuxt/eslint",
+    "@nuxtjs/color-mode",
+    "@nuxtjs/device",
+    "@pinia/nuxt",
+    "@primevue/nuxt-module",
+    "@vueuse/nuxt",
   ],
-})
+  alias: {
+    /**
+     * @fix `ERROR  Cannot find module 'pinia/dist/pinia.mjs'
+     * @source https://stackoverflow.com/a/74801367/16237426
+     */
+    pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs",
+  },
+});
