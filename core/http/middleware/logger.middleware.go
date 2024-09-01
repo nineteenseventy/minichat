5568ce2fd40dceb7ed9a3e50368b4945346a3b17
Marius Kehl <80584154+MeroFuruya@users.noreply.github.com>
2024-09-01 21:39:56 +0200

    update logger middleware

--- core/http/middleware/logger.middleware.go
+++ core/http/middleware/logger.middleware.go
@@ -1,18 +1,55 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/nineteenseventy/minichat/core/logging"
 )
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
+}
 
 func LoggerMiddleware() func(http.Handler) http.Handler {
 	logger := logging.GetLogger("http.middleware.logger")
 
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			logger.Debug().Str("method", r.Method).Str("path", r.URL.Path).Msg("Request received")
-			next.ServeHTTP(w, r)
+			t1 := time.Now()
+
+			lrw := loggingResponseWriter{
+				ResponseWriter: w,
+				statusCode:     http.StatusOK,
+			}
+
+			defer func() {
+				scheme := "http"
+				if r.TLS != nil {
+					scheme = "https"
+				}
+				logger.Debug().
+					Str("from", r.RemoteAddr).
+					Int("status", lrw.statusCode).
+					Str("size", w.Header().Get("Content-Length")).
+					Str("method", r.Method).
+					Str("proto", r.Proto).
+					TimeDiff("time", time.Now(), t1).
+					Msg(fmt.Sprintf(
+						"%s://%s%s",
+						scheme,
+						r.Host,
+						r.RequestURI,
+					))
+			}()
+			next.ServeHTTP(&lrw, r)
 		})
 	}
 }
