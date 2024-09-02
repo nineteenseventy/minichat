44de0dda48e0aa5add9a8ff71081d5953171fe16
Marius Kehl <80584154+MeroFuruya@users.noreply.github.com>
2024-09-02 02:12:42 +0200

    fix eslint config

--- web/src/pages/index.vue
+++ web/src/pages/index.vue
@@ -1,22 +1,15 @@
 <script setup lang="ts">
 import { useApi } from '@/composables/useApi';
 import { useAuth0 } from '@auth0/auth0-vue';
-import { ref, type Ref } from 'vue';
 const { user } = useAuth0();
-const api = useApi();
-
-console.log(user);
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const users: Ref<any> = ref([]);
-api.get('/users').then((response) => {
-  users.value = response.data;
-});
+const { data } = useApi('/users').json();
 </script>
 
 <template>
   <div>{{ user?.email }}</div>
   <ul>
-    <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+    <li v-for="user in data.data" :key="user.id">{{ user.name }}</li>
   </ul>
 </template>
