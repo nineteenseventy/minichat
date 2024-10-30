b6e484cde96ec6ed5d64ab1e0bafd5e67b2fbc10
Marius Kehl <80584154+MeroFuruya@users.noreply.github.com>
2024-10-30 18:01:00 +0100

    update sql migrations

--- database/mig001_profile/mig000_users.ts
+++
@@ -1,9 +0,0 @@
-import type postgres from 'postgres';
-
-export default function (sql: postgres.Sql) {
-  return sql`
-  alter table minichat.users
-  add column bio TEXT,
-  add column username varchar(255) not null
-  `;
-}
