c8bd7091945a30511a9729d8abe533cd44c74083
Marius Kehl <80584154+MeroFuruya@users.noreply.github.com>
2024-10-30 18:23:17 +0100

    fix-database-migration-scripts

--- database/mig000_initial/mig007_channels_private.ts
+++ database/mig000_initial/mig007_channels_private.ts
@@ -4,7 +4,7 @@ export default function (sql: postgres.Sql) {
   return sql`
     create table minichat.channels_private (
       id uuid PRIMARY KEY,
-      title varchar(256) not null
+      title varchar(256) not null,
       created_at timestamp not null default now()
     )
   `;
