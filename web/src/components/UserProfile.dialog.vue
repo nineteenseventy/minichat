d26e6c8d4124071263a3c4834f3cc2a1a5a078c4
Marius Kehl <80584154+MeroFuruya@users.noreply.github.com>
2025-01-09 09:18:46 +0100

    refactor: enhance UserProfile dialog layout and styles with Tailwind CSS

--- web/src/components/UserProfile.dialog.vue
+++ web/src/components/UserProfile.dialog.vue
@@ -48,75 +48,23 @@ const bio = computed(() => {
       <SpinnerComponent />
     </div>
     <UserPictureOnlineStatusComponent :picture="data?.picture" :userId="user" />
-    <span class="username">{{ data?.username }}</span>
-    <span v-if="!!error" class="profile-error"
+    <span class="font-bold text-2xl mix-blend-difference">{{
+      data?.username
+    }}</span>
+    <span v-if="!!error" class="pt-2 text-orange-500"
       >Profile Could not be retrieved!</span
     >
-    <div class="bio">
-      <span class="title">Bio</span>
-      <div class="content">
+    <div
+      class="mt-4 flex flex-col bg-transparent bg-opacity-30 rounded-lg px-2 py-4 min-h-20"
+    >
+      <span class="font-bold mb-2">Bio</span>
+      <div class="flex flex-col">
         <span v-for="line in bio" :key="line">{{ line }}</span>
       </div>
     </div>
-    <div class="controls">
+    <div class="flex gap-4 mt-4">
       <Button @click="close()">Close</Button>
       <Button v-if="isMe" @click="editMyProfile()">Edit</Button>
     </div>
   </div>
 </template>
-
-<style scoped lang="scss">
-.loading {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: rgba(0, 0, 0, 0.5);
-}
-
-.user-profile {
-  display: flex;
-  flex-direction: column;
-  padding: 1rem;
-}
-
-.username {
-  font-weight: bold;
-  font-size: 1.5rem;
-  mix-blend-mode: difference;
-}
-
-.bio {
-  margin-top: 1rem;
-  display: flex;
-  flex-direction: column;
-  background-color: rgba(0, 0, 0, 0.3);
-  border-radius: 0.5rem;
-  padding: 0.5rem 1rem;
-  min-height: 5rem;
-  font-size: 1rem;
-  .title {
-    font-weight: bold;
-    margin-bottom: 0.5rem;
-  }
-  .content {
-    display: flex;
-    flex-direction: column;
-  }
-}
-
-.controls {
-  display: flex;
-  gap: 1rem;
-  margin-top: 1rem;
-}
-
-.profile-error {
-  padding-top: 0.5rem;
-  color: orange;
-}
-</style>
