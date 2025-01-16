d253d93c4c11a1a92e96f73be79010a9e428c71f
CaptainException <113012771+CaptainException@users.noreply.github.com>
2025-01-16 01:28:08 +0100

    feat: add file upload functionality to ChatInputComponent

--- web/src/components/ChatInputComponent.vue
+++ web/src/components/ChatInputComponent.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
-import Button from 'primevue/button';
-import Textarea from 'primevue/textarea';
+import { ref, useTemplateRef } from 'vue';
+import { Button, Textarea, Popover } from 'primevue';
+import { useApi } from '@/composables/useApi';
+import type { Message } from '@/interfaces/message.interface';
 
 defineProps<{
   enableCancel?: boolean;
@@ -18,6 +20,29 @@ async function onKeydown(event: KeyboardEvent) {
     event.preventDefault();
     emit('onSave');
   }
+}
+
+const fileUploadPopOver = useTemplateRef('fileUploadPopOver');
+function showFileAttachmentPopover(event: Event) {
+  fileUploadPopOver.value?.toggle(event);
+}
+
+const fileInputElement = useTemplateRef('fileInputElement');
+async function submitImage() {
+  if (!fileInputElement.value || !fileInputElement.value.files) return;
+  const file = fileInputElement.value.files[0];
+  if (!file) return;
+  const { error } = await useApi(`/messages/${id}`).post(file).json<Message>();
+  if (error) console.log('Error posting image: ', error);
+  fileUploadPopOver.value?.hide();
+}
+
+const newImageSrc = ref('');
+function onFileChange() {
+  if (!fileInputElement.value || !fileInputElement.value.files) return;
+  const file = fileInputElement.value.files[0];
+  if (!file) return;
+  newImageSrc.value = URL.createObjectURL(file);
 }
 </script>
 
@@ -39,10 +64,29 @@ async function onKeydown(event: KeyboardEvent) {
       @click="emit('onCancel')"
     />
     <Button
+      icon="pi pi-paperclip"
+      class="min-h-11 min-w-11"
+      severity="help"
+      @click="showFileAttachmentPopover"
+    />
+    <Button
       icon="pi pi-send"
       class="self-end min-h-11 min-w-11"
       @click="emit('onSave')"
     />
+    <Popover ref="fileUploadPopOver">
+      <div class="flex flex-col items-center gap-4">
+        <input
+          ref="fileInputElement"
+          @change="onFileChange"
+          type="file"
+          accept="image/*"
+          class="w-full"
+        />
+
+        <Button @click="submitImage" class="self-end">Submit</Button>
+      </div>
+    </Popover>
   </div>
 </template>
 
